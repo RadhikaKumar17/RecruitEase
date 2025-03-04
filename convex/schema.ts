e6c320f48dc5b289a5b0de5e3f36ae4ef356e92e
RadhikaKumar17 <radhika70428@gmail.com>
2025-03-04 19:20:38 +0530

    setted up clerk, stream and convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,12 @@
-// import {defineSchema} from '@convex/server';
-// export default defineSchema({
-//   users: defineTable({
-//     name: 'users', 
-// });
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
   users: defineTable({
-    name: v.string(), // Define 'name' with a string type
-  })
+    name: v.string(),
+    email: v.string(),
+    image: v.optional(v.string()),
+    role: v.union(v.literal("candidate"), v.literal("interviewer")), //candidate or interviewer
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"]),
 });
